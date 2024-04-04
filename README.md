9046d4b2c63fafaa95a982ef6fdcb380e4d1a1b4
dhowe <dhowe@cims.nyu.edu>
2024-04-05 01:01:36 +0800

    initial

--- README.md
+++ README.md
@@ -1,11 +1,5 @@
-# Emoji Substitution
+# Emoji-Sub
 
-**This add-on injects JavaScript into web pages. The `addons.mozilla.org` domain disallows this operation, so this add-on will not work properly when it's run on pages in the `addons.mozilla.org` domain.**
+Replaces words that describe an emoji with the emoji itself. This runs as a content script and scans web pages, looking for text that can be replaced with emoji.
 
-## What it does
-
-Replaces words that describe an emoji with the emoji itself. This runs as a content script and scans web pages, looking for text that can be replaced with emoji. As an example, after installing visit https://mozilla.org and notice that the text "firefox" should change.
-
-## What it shows
-
-A good example for beginners that can be used as a "make your first add-on" tutorial and / or referenced to create other add-ons.
+Based on examples [here](https://github.com/mdn/webextensions-examples)
--- sub-emoji.js
+++ sub-emoji.js
@@ -1,24 +1,22 @@
-console.log('sub-emoji.js injected as content-script');
+
+console.log('content-script: sub-emoji.js');
 
 let emojis = { apple: '🍎', banana: '🍌', bang: '💥', baseball: '⚾', basketball: '🏀', beer: '🍺', bicycle: '🚴', bike: '🚴', bomb: '💣', boy: '👦', bug: '🐛', burger: '🍔', burn: '🔥', cake: '🎂', candy: '🍬', cat: '🐱', celebration: '🎉', cheeseburger: '🍔', cookie: '🍪', cool: '😎', cry: '😢', dog: '🐶', doge: '🐕', earth: '🌎', explode: '💥', fart: '💨', fast: '💨', female: '👩', fire: '🔥', fish: '🐟', flame: '🔥', flower: '🌹', food: '🍕', football: '🏈', girl: '👧', golf: '⛳', hamburger: '🍔', happy: '😀', horse: '🐴', hot: '🔥', kiss: '😘', laugh: '😂', lit: '🔥', lock: '🔒', lol: '😂', love: '😍', male: '👨', man: '👨', monkey: '🐵', moon: '🌙', note: '📝', paint: '🎨', panda: '🐼', party: '🎉', pig: '🐷', pizza: '🍕', planet: '🌎', rose: '🌹', rofl: '😂', sad: '😢', sleep: '😴', smile: '😀', smiley: '😀', soccer: '⚽', star: '⭐', sun: '☀️', sunglasses: '😎', surprised: '😮', tree: '🌲', trophy: '🏆', win: '🏆', wind: '💨', wine: '🍷', wink: '😉', woman: '👩', world: '🌎', wow: '😮' };
 
 // sort by length of word to avoid replacing substrings
-emojis = Object.keys(emojis).sort().reduce((obj, key) => (obj[key] = emojis[key], obj), {});
+emojis = Object.keys(emojis).sort().reduce((o, w) => (o[w] = emojis[w], o), {});
 
-// create an object with regexs for each word { word: regex }
-let regexs = Object.keys(emojis).reduce((obj, word) => (obj[word] = new RegExp('\\b' + word + '\\b', 'gi'), obj), {});
-
-function replaceText(node) { // handles all the replacements
+function replaceText(node) { // this handles the replacements
 
   if (node.nodeType === Node.TEXT_NODE) {
 
     // skip textareas to be safe with forms
     if (node?.parentNode.nodeName === 'TEXTAREA') return;
 
-    // use 'regexs' to replace each 'word' with its emoji
+    // replace each 'word' with its emoji
     let text = node.textContent;
     Object.entries(emojis).forEach(([word, emoji]) => {
-      text = text.replace(regexs[word], emoji);
+      text = text.replaceAll(word, emoji);
     });
     node.textContent = text;
   }
@@ -27,14 +25,6 @@ function replaceText(node) { // handles all the replacements
     node.childNodes.forEach(replaceText);
   }
 }
-
-// apply our code to any new nodes added to the page
-new MutationObserver(muts => muts.forEach
-  (mut => mut?.addedNodes.forEach(node => replaceText(node))))
-  .observe(document.body, { childList: true, subtree: true });
-
-
-replaceText(document.body); // start from the body tag
-
+replaceText(document.body); // call our function, starting from body tag
 
 
